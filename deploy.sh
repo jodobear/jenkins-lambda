25616c77262304ef19ef53f6f5f63ac94107ac5d
jodobear <jodobear@protonmail.com>
2020-05-14 15:12:52 +0200

    created zip locally

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -e
-rm -f go-lambda.zip && zip -rq go-lambda .
+# rm -f go-lambda.zip && zip -rq go-lambda .
 aws lambda update-function-code --function-name go-lambda --zip-file
 fileb://go-lambda.zip
