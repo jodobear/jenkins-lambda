7ae9ef2ab67aa06586340e64504a97f7109e58e4
jodobear <jodobear@protonmail.com>
2020-05-14 15:37:56 +0200

    created fn manually and updating from jenkins

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 set -e
 # rm -f go-lambda.zip && zip -rq go-lambda .
-aws lambda create-function --function-name go-lambda --zip-file fileb://go-lambda.zip
+aws lambda update-function-code --function-name go-lambda --zip-file fileb://go-lambda.zip
